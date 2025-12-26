a7d22aaa61a210f1801cbba3ee852eb8523d436b
adisty <distyyfaa@gmail.com>
2025-12-26 17:41:30 +0700

    add structure dosen

---
+++ include/dosen.h
@@ -0,0 +1,14 @@
+#ifndef DOSEN_H_INCLUDED
+#define DOSEN_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+#define first(L) L.first
+#define last(L) L.last
+#define next(P) P->next
+#define prev(P) P->prev
+#define info(P) P->info
+
+
+#endif

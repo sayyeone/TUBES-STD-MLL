18249ef572d6bf9b2eec387aaa655deb3a8159e1
adisty <distyyfaa@gmail.com>
2025-12-26 18:46:15 +0700

    add void

--- include/dosen.h
+++ include/dosen.h
@@ -30,4 +30,16 @@ struct List_dosen {
     address_dosen last;
 };
 
+void createList(List_dosen &L);
+address_dosen alokasi_dosen(string nama, string nip, string keahlian);
+void dealokasi_dosen(address_dosen &P);
+
+void insertLast(List_dosen &L, address_dosen P);
+void deleteFirst(List_dosen &L, address_dosen &P);
+void deleteAfter(List_dosen &L, address_dosen Prec, address_dosen &P);
+
+address_dosen findElmDosen(List_dosen L, string nama);
+address_dosen findElmDosenByNIP(List_dosen L, string nip);
+void printInfo(List_dosen L);
+
 #endif

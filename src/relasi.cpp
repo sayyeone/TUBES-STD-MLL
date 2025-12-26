7855f86a0f4ee95149a2671e8539af77c21343bf
adisty <distyyfaa@gmail.com>
2025-12-26 19:59:53 +0700

    add void

---
+++ src/relasi.cpp
@@ -0,0 +1,61 @@
+#include "../include/relasi.h"
+
+address_relasi alokasi_relasi(address_dosen D, address_mahasiswa M) {
+
+}
+
+void dealokasi_relasi(address_relasi &R) {
+
+}
+
+void connect(address_dosen D, address_mahasiswa M, address_relasi &firstRel) {
+
+}
+
+void disconnect(address_dosen D, address_mahasiswa M, address_relasi &firstRel) {
+
+}
+
+void deleteAllRelasiDosen(address_dosen D, address_relasi &firstRel) {
+
+}
+
+void deleteAllRelasiMahasiswa(address_mahasiswa M, address_relasi &firstRel) {
+
+}
+
+address_relasi findRelasiDosenMahasiswa(address_dosen D, address_mahasiswa M, address_relasi firstRel) {
+
+}
+
+address_relasi findFirstRelasiDosen(address_dosen D, address_relasi firstRel) {
+
+}
+
+address_relasi findFirstRelasiMahasiswa(address_mahasiswa M, address_relasi firstRel) {
+
+}
+
+int countMahasiswaBimbingan(address_dosen D, address_relasi firstRel) {
+
+}
+
+int countDosenPembimbing(address_mahasiswa M, address_relasi firstRel) {
+
+}
+
+void showAllMahasiswaByDosen(address_dosen D, address_relasi firstRel) {
+
+}
+
+void showAllDosenByMahasiswa(address_mahasiswa M, address_relasi firstRel) {
+
+}
+
+void showAllData(List_mahasiswa LM, address_relasi firstRel) {
+
+}
+
+void findDosenMaxMinBimbingan(List_dosen LD, address_relasi firstRel) {
+
+}

02efb8e3f04a6be4945d6571553044cde6d6dcc2
adisty <distyyfaa@gmail.com>
2025-12-26 19:51:01 +0700

    add void

--- include/relasi.h
+++ include/relasi.h
@@ -13,7 +13,34 @@ using namespace std;
 
 typedef struct elmlist_relasi *address_relasi;
 
+struct elmlist_relasi {
+    address_dosen dosen;
+    address_mahasiswa mahasiswa;
+    address_relasi nextDosen;
+    address_relasi nextMhs;
+};
 
+address_relasi alokasi_relasi(address_dosen D, address_mahasiswa M);
+void dealokasi_relasi(address_relasi &R);
+
+void connect(address_dosen D, address_mahasiswa M, address_relasi &firstRel);
+void disconnect(address_dosen D, address_mahasiswa M, address_relasi &firstRel);
+
+void deleteAllRelasiDosen(address_dosen D, address_relasi &firstRel);
+void deleteAllRelasiMahasiswa(address_mahasiswa M, address_relasi &firstRel);
+
+address_relasi findRelasiDosenMahasiswa(address_dosen D, address_mahasiswa M, address_relasi firstRel);
+address_relasi findFirstRelasiDosen(address_dosen D, address_relasi firstRel);
+address_relasi findFirstRelasiMahasiswa(address_mahasiswa M, address_relasi firstRel);
+
+int countMahasiswaBimbingan(address_dosen D, address_relasi firstRel);
+int countDosenPembimbing(address_mahasiswa M, address_relasi firstRel);
+
+void showAllMahasiswaByDosen(address_dosen D, address_relasi firstRel);
+void showAllDosenByMahasiswa(address_mahasiswa M, address_relasi firstRel);
+void showAllData(List_mahasiswa LM, address_relasi firstRel);
+
+void findDosenMaxMinBimbingan(List_dosen LD, address_relasi firstRel);
 
 
 
